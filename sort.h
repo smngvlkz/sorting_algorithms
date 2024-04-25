9003bd56452b705600e50adfe48a2404f34e527f
smngvlkz <asmdigital14@gmail.com>
2024-04-25 01:53:51 -0700

    Bubble sort

--- sort.h
+++ sort.h
@@ -18,6 +18,7 @@ typedef struct listint_s
 } listint_t;
 
 /* PROTOTYPES */
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
